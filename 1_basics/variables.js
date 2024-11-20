8d72fa795468b98b7484d7231b05f76aa34b86e7
Pooja Pansare <136827234+PoojaPansare@users.noreply.github.com>
2024-11-20 20:41:00 +0530

    Add files via upload

---
+++ 1_basics/variables.js
@@ -0,0 +1,18 @@
+const accountId =  12323
+let account_email = "pansarepooja64@gmail.com"
+var account_password = "ppc@2809"
+let account_city = "Pune"
+let account_type
+//accounttype doesnt have any value so it will give undefined as output
+
+// accountId = 2 not allowed because accountId is constant ie. its value cannot be changed its fixed
+//Constant is declared with const keyword value wont change
+//Varaible are declared in 2 ways in js 
+// 1st way ---- var ---- its not used mostly bcoz of functional scope pb
+// 2nd way ----- let ----- its used mostly  
+console.log( account_type)
+
+console.table([accountId, account_email, account_city])
+
+// here console.table will give output in table format 
+// console.log will just print it
